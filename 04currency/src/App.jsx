0bcc5ad7e1d0bbca34c2c77c7dbc58266f0c17cb
Akshay More <moreakshay725@gmail.com>
2023-11-22 22:22:52 +0530

    currency02

--- 04currency/src/App.jsx
+++ 04currency/src/App.jsx
@@ -1,24 +1,13 @@
-import React,  { useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState({})
-  useEffect(() => {
-     fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr.json`)
-     .then(res => res.json())
-     .then(setData(res[inr]))
-     console.log("PPPP ", data);
- 
-  },[])
- 
- 
+  const [count, setCount] = useState(0);
+
   return (
-   <div>
-    <h1>asasas</h1>
-    <p>{data}</p>
-   </div>
-  )
+    <div>
+      <h1 className="text-3xl">Currency</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 04currency/src/hooks/useCurrency.js
+++ 04currency/src/hooks/useCurrency.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 
-
 function useCurrency(currency) {
-
- const [data, setData] = useState({})
- useEffect(() => {
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-    .then(res => res.json())
-    .then(setData(res[currency]))
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
     console.log(data);
+  }, [currency]);
 
-
- },[currency])
-
-
+  return data;
 }
+
+export default useCurrency;
